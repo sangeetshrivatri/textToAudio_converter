8a91d27a2fa9399c8489af2e7b86e7369dfffc6a
Sangeet Shrivatri <Sangeet.Shrivatri@ATMECS.CORP>
2022-03-18 21:26:08 +0530

    Font Size changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,7 +8,7 @@ function Main() {
   return (
     <div className="Main">
        <Container textAlign='center' text>
-    <Header className="heading" textAlign='center' as='h2'>Article </Header>
+    <Header className="heading" textAlign='center' as='h1'>The Article goes here.</Header>
     <Image size='big' centered src='https://img.freepik.com/free-vector/sound-wave-with-imitation-sound-audio-identification-technology_106065-64.jpg' />
      <Divider hidden  />
      <SubComponent />
